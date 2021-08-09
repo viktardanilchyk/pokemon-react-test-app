732b9c1edd287603da5e9c7c1943b6c4715017a4
viktardanilchyk <viktardanilchyk@gmail.com>
2021-08-09 16:48:06 +0300

    add saga

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,20 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { applyMiddleware, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = configureStore({
-  reducer: {},
-});
+import rootReducer from '../reducers';
+import rootSaga from '../sagas';
+
+export default function configureStore(initialState) {
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
+
+  const enhancers = [middlewareEnhancer];
+  const composedEnhancers = composeWithDevTools(...enhancers);
+
+  const store = createStore(rootReducer, initialState, composedEnhancers);
+
+  sagaMiddleware.run(rootSaga);
+  return store;
+}
